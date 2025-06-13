4e78158149f2abdbe67a52f65dd908bbbdf1a502
AGUENAOU-AYOUB <aguenaouayoub04@gmail.com>
2025-06-13 18:44:43 +0100

    Handle CSRF for login

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 import os
 
+from . import csrf
+
 auth_bp = Blueprint('auth', __name__)
 
 ADMIN_USER = os.getenv('ADMIN_USERNAME', 'admin')
 ADMIN_PASS = os.getenv('ADMIN_PASSWORD', 'password')
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
+@csrf.exempt
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
